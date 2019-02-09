267cc915ac745b6aef12ff6259e2d0c06f09e322
vishnuram <vrama2195@gmail.com>
2019-02-09 23:47:43 +0530

    Add project files.

---
+++ Login/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using System.IO;
+
+namespace Login
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity { 
+        EditText username;  
+        EditText password;
+        Button loginbt;
+        string path = Application.Context.FilesDir.Path;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            string filePath = Path.Combine(path, "test1.txt");
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            username = FindViewById<EditText>(Resource.Id.username);
+            password = FindViewById<EditText>(Resource.Id.txtPassword);
+            loginbt = FindViewById<Button>(Resource.Id.btnLogin);
+            loginbt.Click += delegate {
+                if (username.Text == "Vishnu"|| username.Text == "Joshua" && password.Text == "12345")
+                {
+                    Toast.MakeText(this, "Login successfully done!", ToastLength.Long).Show();
+                    var intent = new Intent(this, typeof(SecondActivity));
+                    intent.PutExtra("DATA_PASS", username.Text);
+                    intent.PutExtra("PATH", filePath);//DATA_PASS is Identify the Value Pass variable  
+                    this.StartActivity(intent);
+                }
+                else
+                {
+                      
+                    Toast.MakeText(this, "Wrong credentials found!", ToastLength.Long).Show();
+                }
+
+            };
+        }
+
+    }
+}
---
+++ Login/SecondActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Android.Content;
+using Android.App;
+
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Login
+{
+    
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+
+
+        
+        Button btngetvalue;
+        TextView txt1,txt2;
+        string name;
+        EditText ed1;
+       // string path = Application.Context.FilesDir.Path;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SecondLayout);
+            name = Intent.GetStringExtra("DATA_PASS"); //Get The Value  
+           string path = Intent.GetStringExtra("PATH");
+            btngetvalue = FindViewById<Button>(Resource.Id.btnclick);
+            txt1= FindViewById<TextView>(Resource.Id.label);
+            txt2 = FindViewById<TextView>(Resource.Id.label2);
+            ed1 = FindViewById<EditText>(Resource.Id.name);
+            //ISharedPreferences localnames = Application.Context.GetSharedPreferences("USERNAMES", FileCreationMode.Private);
+            //ISharedPreferencesEditor localedit = localnames.Edit();
+            txt1.Text = txt1.Text+" "+name;
+            
+            btngetvalue.Click += delegate
+            {
+                
+                //txt2.Text = txt2.Text + " " + localnames.GetString("name", null);
+                //localedit.PutString("name", ed1.Text);
+                //localedit.Commit();
+                System.IO.File.AppendAllText(path, " "+ed1.Text);
+                // txt2.Text = txt2.Text + " " + localnames.GetString("name", null);
+               String data= System.IO.File.ReadAllText(path);
+                txt2.Text = data;
+               
+            };
+        
+    }
+    }
+}
